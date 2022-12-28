f654020fc90b187c7971f02be96c76eb5d9b3912
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2022-12-28 17:18:28 +0700

    Add project files.

---
+++ BinarySearchTree_078/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree_078
+{
+    class Node
+    {
+        public string info;
+        public string leftchild;
+        public string rightchild;
+
+        // Constructor for the Node Class
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
+    /* A node class consists of the three things, the information,
+     * references to the right child, references to the right child, and references to the left child*/
+
+    class Program
+    {
+        public Node ROOT;
+        public Program()
+        {
+            ROOT = null; /*Initializing ROOT to null */
+        }
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            /* This function search the currentNode of the specified Node as well as the current Node of its parents */
+            currentNode = ROOT;
+            parent = null;
+            while((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
+        }
+        public void insert(string element)/* Insert a node in the binary search tree */
+    }
+}
